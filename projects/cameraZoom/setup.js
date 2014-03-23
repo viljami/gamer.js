4d3e5a8c1564488bb0f51c02547c6c3ffdc795a1
viljami <viljami.peltola@iki.fi>
2014-03-23 18:18:15 +0000

    Introduced lib folder.

--- projects/cameraZoom/setup.js
+++
@@ -1,9 +0,0 @@
-;(function(undefined){
-    var canvas = document.getElementsByTagName('canvas')[0];
-    var resize = function(){
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    };
-    window.onresize = resize;
-    resize();
-})();
