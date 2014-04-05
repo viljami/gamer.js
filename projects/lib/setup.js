26800092ce0655530477b24b3ba831c3c6f69f21
viljami <viljami.peltola@iki.fi>
2014-04-05 16:57:47 +0100

    Improved page dimension detection for mobile devices.

--- projects/lib/setup.js
+++ projects/lib/setup.js
@@ -1,9 +1,26 @@
 ;(function(undefined){
     var canvas = document.getElementsByTagName('canvas')[0];
+
+    // Sometimes window.innerWidth has delay...
+    // This method has not.
+    var dimensionsDiv = document.createElement('div');
+    dimensionsDiv.style.cssText = [
+        'position: absolute',
+        'top: 0px',
+        'bottom: 0px',
+        'left: 0px',
+        'right: 0px',
+        'z-index: -999'
+    ].join(';');
+
+    document.body.appendChild(dimensionsDiv);
+
     var resize = function(){
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+        var dimenensions = dimensionsDiv.getBoundingClientRect();
+        canvas.width = dimenensions.width;
+        canvas.height = dimenensions.height;
     };
+
     window.onresize = resize;
     resize();
 })();
