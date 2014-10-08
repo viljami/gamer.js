4ad5f2a8ccceb5cb4d20bbc4dc93bbfdc1608f77
Viljami Peltola <viljami.peltola@iki.fi>
2014-10-08 18:20:14 +0100

    Build.

--- jump/build/app.js
+++ jump/build/app.js
@@ -1989,87 +1989,6 @@ define('materials',[
   };
 });
 
-
-/*
-
-Texture repeat
-http://stemkoski.github.io/Three.js/Texture-Repeat.html
-
-// FLOOR
-  var floorTexture = new THREE.ImageUtils.loadTexture( 'images/checkerboard.jpg' );
-  floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
-  floorTexture.repeat.set( 10, 10 );
-  var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
-  var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
-  var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-  floor.position.y = -0.5;
-  floor.rotation.x = Math.PI / 2;
-  scene.add(floor);
-  // SKYBOX/FOG
-  var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
-  var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
-  var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
-  // scene.add(skyBox);
-  scene.fog = new THREE.FogExp2( 0x9999ff, 0.00025 );
-
-  ////////////
-  // CUSTOM //
-  ////////////
-
-  // Spheres
-  //   Note: a standard flat rectangular image will look distorted,
-  //   a "spherical projection" image will look "normal".
-
-  // radius, segmentsWidth, segmentsHeight
-  var sphereGeom =  new THREE.SphereGeometry( 40, 32, 16 );
-
-  var light2 = new THREE.AmbientLight(0x444444);
-  scene.add(light2);
-
-  // basic lava ball
-  var lavaTexture = THREE.ImageUtils.loadTexture( 'images/lava.jpg' );
-  var lavaMaterial = new THREE.MeshBasicMaterial( { map: lavaTexture } );
-  var lavaBall = new THREE.Mesh( sphereGeom.clone(), lavaMaterial );
-  lavaBall.position.set(-100, 50, 0);
-  scene.add( lavaBall );
-
-  // texture repeated twice in each direction
-  var lavaTexture = THREE.ImageUtils.loadTexture( 'images/lava.jpg' );
-  lavaTexture.wrapS = lavaTexture.wrapT = THREE.RepeatWrapping;
-  lavaTexture.repeat.set( 2, 2 );
-  var lavaMaterial = new THREE.MeshBasicMaterial( { map: lavaTexture } );
-  var lavaBall = new THREE.Mesh( sphereGeom.clone(), lavaMaterial );
-  lavaBall.position.set(0, 50, 0);
-  scene.add( lavaBall );
-
-  // texture repeated thrice in each direction
-  var lavaTexture = THREE.ImageUtils.loadTexture( 'images/lava.jpg' );
-  lavaTexture.wrapS = lavaTexture.wrapT = THREE.RepeatWrapping;
-  lavaTexture.repeat.set( 3, 3 );
-  var lavaMaterial = new THREE.MeshBasicMaterial( { map: lavaTexture } );
-  var lavaBall = new THREE.Mesh( sphereGeom.clone(), lavaMaterial );
-  lavaBall.position.set(100, 50, 0);
-  scene.add( lavaBall );
-
-  // Cubes
-  //   Note: when using a single image, it will appear on each of the faces.
-
-  var cubeGeometry = new THREE.CubeGeometry( 85, 85, 85 );
-
-  var crateTexture = new THREE.ImageUtils.loadTexture( 'images/crate.gif' );
-  var crateMaterial = new THREE.MeshBasicMaterial( { map: crateTexture } );
-  var crate = new THREE.Mesh( cubeGeometry.clone(), crateMaterial );
-  crate.position.set(-60, 50, -100);
-  scene.add( crate );
-
-  var crateTexture = new THREE.ImageUtils.loadTexture( 'images/crate.gif' );
-  crateTexture.wrapS = crateTexture.wrapT = THREE.RepeatWrapping;
-  crateTexture.repeat.set( 5, 5 );
-  var crateMaterial = new THREE.MeshBasicMaterial( { map: crateTexture } );
-  var crate = new THREE.Mesh( cubeGeometry.clone(), crateMaterial );
-  crate.position.set(60, 50, -100);
-  scene.add( crate );
-*/;
 define('config',[], function(){
   return {
     depth: 1,
@@ -8041,7 +7960,7 @@ require([
   };
 
   onload(function(){
-    document.body.innerHTML = '';
+    document.body.innerHTML = '<audio src="assets/musa.mp3" preload="auto" autoplay loop controls style="position:absolute;bottom:0;left:0;z-index:100;"></audio>';
 
     world.init();
 
